939bd49266b563a8907d028d6c4e66f353d4bdfd
Ethan Trantalis <github@etcyber.org>
2025-06-01 13:43:32 -0700

    added file writing for each cluster

--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,10 @@
 import numpy as np
+import csv
+import os
+
+outputDir = "finalClusters"
 
 # Assuming a csv file where each line is a centroid with n dimensions
-
 centroids = np.genfromtxt('centroids.csv', delimiter=',')
 data = np.genfromtxt('data.csv', delimiter=',')
 
@@ -9,7 +12,14 @@ data = np.genfromtxt('data.csv', delimiter=',')
 clusters = [[] for _ in range(centroids.shape[0])]
 
 totalConverged = 0
-while totalConverged < centroids.shape[0]:
+maxIterations = 100
+totalIterations = 0
+while (totalConverged < centroids.shape[0]) and (totalIterations < maxIterations):
+
+    # Reset the cluster arrays and reset the totalConverged for each iteration
+    totalConverged = 0
+    clusters = [[] for _ in range(centroids.shape[0])]
+
     for dp in data:
         # Calculate the norm for each row
         distances = np.linalg.norm(dp - centroids, axis=1)
@@ -25,8 +35,8 @@ while totalConverged < centroids.shape[0]:
 
         if len(clusters[i]) > 0:
 
-            centroidOld = np.array(centroids[i])
-            centroidNew = np.array(np.mean(clusters[i], axis=0))
+            centroidOld = centroids[i].copy()
+            centroidNew = np.mean(clusters[i], axis=0)
 
             converged = np.allclose(centroidOld, centroidNew, atol=1e-4)
 
@@ -35,5 +45,19 @@ while totalConverged < centroids.shape[0]:
             else:
                 centroids[i] = centroidNew
         else:
+
+            # Counting a cluster with no pints as converged
             totalConverged += 1
+
+    if totalConverged == centroids.shape[0]:
+        break
+    else:
+        totalIterations += 1
+
+for i, c in enumerate(clusters):
+
+    with open(os.path.join(outputDir, f"cluster{i}.csv"), 'w', newline='') as f:
+
+        csvFile = csv.writer(f)
+        csvFile.writerows(c)
 
